786765fd56b8ffb939da3a02f95fe382765a2fe3
LNDavid <ldavid@rmacan.com>
2022-04-11 14:25:55 -0400

    initial lines of code

---
+++ app.js
@@ -0,0 +1,20 @@
+const commandLineArgs = process.argv;
+
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);   
+
+const printProfileData = (profileDataArr) => {
+
+    // This
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+
+        console.log(profileDataArr);
+    }
+
+    console.log("==============");
+
+    // is the same as this...
+    profileDataArr.forEach((profileItem) => console.log(profileItem));
+};
+
+printProfileData(profileDataArgs);
